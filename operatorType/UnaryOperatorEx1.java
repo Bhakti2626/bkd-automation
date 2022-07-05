2eb44577a3bc5a2e4af7927e425663ff123e0e0a
Bhakti2626 <106881101+Bhakti2626@users.noreply.github.com>
2022-07-05 12:06:54 +0530

    Create UnaryOperatorEx1.java

---
+++ operatorType/UnaryOperatorEx1.java
@@ -0,0 +1,22 @@
+package operatorType;
+
+public class UnaryOperatorEx1 {
+	
+	public static void main(String args[]) {
+		
+//		int a= 10;
+//		System.out.println(a);
+//		a++;                  //a=a+1=10+1=11
+//		System.out.println(a);
+//		a++;                  // a=a+1=11+1=12
+//		System.out.println(a);
+		
+		int b= 20;
+		System.out.println(b);
+		b--;                 //b=b-1= 20-1=19
+		System.out.println(b);
+		b--;                //b=b-1=19-1=18
+		System.out.println(b);
+	}
+
+}
