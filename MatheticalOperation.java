5625046035a96cbb2c10ae17187064b421c5ec28
Bhakti2626 <106881101+Bhakti2626@users.noreply.github.com>
2022-07-05 11:59:27 +0530

    Create MatheticalOperation.java

---
+++ MatheticalOperation.java
@@ -0,0 +1,15 @@
+package mathematicalOperations;
+
+public class MatheticalOperation {
+	
+	public static void main(String args[]) {
+		
+		System.out.println(10+20);
+		System.out.println(20-10);
+		System.out.println(10*20);
+		System.out.println(20/10);
+		System.out.println(20%10);
+		
+	}
+
+}
