35f7d7543426e2e63559e1e2fbbc847bd3a2b7e0
Bhakti2626 <106881101+Bhakti2626@users.noreply.github.com>
2022-07-05 11:57:40 +0530

    Create DatatypeExample.java

---
+++ DatatypeExample.java
@@ -0,0 +1,47 @@
+package firstDatatype;
+
+public class DatatypeExample {
+	
+	public static void main(String args[]) {
+		
+		boolean a; //variable declaration statrt with small letter
+		a= true;  // variable assignment 
+		System.out.println(a);
+	
+		boolean a1 = false; //variable declaration with assignment
+		
+		System.out.println(a);
+		System.out.println(a1);
+		
+		// byte data type
+		
+		byte b1 = 50;
+		System.out.println(b1);
+		
+		byte b2 = 127;
+		byte b3 = 100;
+		System.out.println(b2 + b3);
+		
+		// short datatype
+		short s1 = 31500;
+		System.out.println(s1);
+		
+		// int datatype
+		
+		int i = 223890;
+		System.out.println(i);
+		
+		int i1 = -245679;
+		System.out.println(i1);
+		
+		//
+		
+		char ch = 's';
+		
+		String sh = "Shiv";
+		
+	}
+	
+
+
+}
