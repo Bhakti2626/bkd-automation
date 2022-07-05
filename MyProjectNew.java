6413d46077e58fe1d063c0702feb7cb7e515fe32
Bhakti2626 <106881101+Bhakti2626@users.noreply.github.com>
2022-07-05 12:04:36 +0530

    Create MyProjectNew.java

---
+++ MyProjectNew.java
@@ -0,0 +1,15 @@
+
+
+public class MyProjectNew {
+
+	public static void main(String args[]) {
+
+		System.out.println("Name : " + "Bhakti Darandale" + "\nAddress : " + "Pune" + "\nEdu : " + "BE");
+
+		System.out.println("Name : " + "Bhakti Darandale");
+		System.out.println("Address : " + "Pune");
+		System.out.println("Edu : " + "BE");
+
+	}
+
+}
