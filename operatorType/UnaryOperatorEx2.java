5b763510108dad9660137de445f339acf1a30d51
Bhakti2626 <106881101+Bhakti2626@users.noreply.github.com>
2022-07-05 12:07:24 +0530

    Create UnaryOperatorEx2.java

---
+++ operatorType/UnaryOperatorEx2.java
@@ -0,0 +1,37 @@
+package operatorType;
+
+public class UnaryOperatorEx2 {
+	
+	
+	public static void main(String args[]) {
+		
+//		int a= 10;
+//		System.out.println(a);
+//		++a;                 //a=1+a=1+10=11
+//		System.out.println(a);
+//		++a;                //a=1+a=a+11=12
+//		System.out.println(a);
+//		
+//		int b=20;
+//		System.out.println(b);
+//		--b;                 //b=b-1=20-1=19
+//		System.out.println(b);
+//		--b;                //b=b-1=19-1=18
+//		System.out.println(b);
+		
+		float c= 10.8f;
+		c++;       
+		System.out.println(c);
+		
+		double d = 10.5;
+		d++;
+		System.out.println(d);
+		
+		char e = 'c';
+		e++;             //e=c+1=d
+		System.out.println(e);
+		e--;            //e=d-1=c
+		System.out.println(e);
+	}
+
+}
