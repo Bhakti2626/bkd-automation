681e26a74d8b3d3dd1e52d46d3fb6e6c9b9ee3f4
Bhakti2626 <106881101+Bhakti2626@users.noreply.github.com>
2022-07-05 12:06:04 +0530

    Create ArthOperator.java

---
+++ operatorType/ArthOperator.java
@@ -0,0 +1,21 @@
+package operatorType;
+
+public class ArthOperator {
+	
+	public static void main(String args[]) {
+		
+		int a = 20;
+		int b = 7;
+		
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+		
+		System.out.println(50/15);
+		System.out.println(50%15);
+		
+	}
+
+}
