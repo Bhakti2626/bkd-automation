4e47e477441b4bb6a962a975564aa3c8a71d9652
Bhakti2626 <106881101+Bhakti2626@users.noreply.github.com>
2022-07-05 12:05:08 +0530

    Create MySecondProgram.java

---
+++ MySecondProgram.java
@@ -0,0 +1,16 @@
+package myPackage;
+
+public class MySecondProgram {
+	
+	
+	public static void main(String args[]) {
+		//System.out.println("*" + "\n**"+ "\n***"+ "\n****"+ "\n*****");
+		for(int i=0; i<10; i++) {
+			
+			for (int j=0; j<=i;j++) {
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+	}
+}
