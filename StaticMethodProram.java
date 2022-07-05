0db475cbd0e784d8e95cd2550338af8b3dc2cf53
Bhakti2626 <106881101+Bhakti2626@users.noreply.github.com>
2022-07-05 12:09:02 +0530

    Create StaticMethodProram.java

---
+++ StaticMethodProram.java
@@ -0,0 +1,16 @@
+
+
+public class StaticMethodProram {
+	
+	public static void main(String args[]) {
+		StaticProram1.bhakti();
+		
+		System.out.println("Static Program");
+	}
+	
+	public static void bhakti(){
+		System.out.println("Static Method Executed");
+		
+	}
+
+}
